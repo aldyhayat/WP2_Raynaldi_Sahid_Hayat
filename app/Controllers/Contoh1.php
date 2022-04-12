2598345f61acd442dd27af7b359b15169343a42a
aldyhayat <hayat.raynaldi@gmail.com>
2022-04-12 12:05:11 +0700

    Update Projek

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace App\Controllers;
+use CodeIgniter\Controller;
+
+class Contoh1 extends Controller{
+
+    public function index(){
+        echo '<h1>Perkenalkan';
+        echo '<p>Nama saya Raynaldi Sahid Hayat,saya tinggal di daerah depok, olahraga yang saya sukai adalah futsal';
+    }
+
+} 
+
+
+
+
+
+?>
